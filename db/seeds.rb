ce1e9b0976bfadb7ff151c41ea24eceb0b8172c6
Patrick Ofilada <pcofilada@gmail.com>
2017-07-02 14:00:39 +0800

    Fix seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@ parking_lots.each do |parking|
     initial_rate: parking['initial_rate'],
     succeeding_rate: parking['succeeding_rate'],
     lat: parking['lat'],
-    long: parking['lng']
+    long: parking['long']
   )
 end
